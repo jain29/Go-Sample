739d00310b416fa8690684ab2c701e010043f973
Gaurav Jain <GAURAV.JAIN1@pb.com>
2020-03-26 13:43:15 +0530

    code changes

--- model/batchdb.go
+++ model/batchdb.go
@@ -1,17 +1,16 @@
 package model
 
 type BatchDB struct {
-
-	OrderID    	    string `json:"OrderID,omitempty" bson:"OrderID,omitempty"`
-	CompanyName 	string `json:"CompanyName,omitempty" bson:"CompanyName,omitempty"`
-	Address1        string `json:"Address1,omitempty" bson:"Address1,omitempty"`
-	Address2        string `json:"Address2,omitempty" bson:"Address2,omitempty"`
-	Address3        string `json:"Address3,omitempty" bson:"Address3,omitempty"`
-	City       		string `json:"City,omitempty" bson:"City,omitempty"`
-	State       	string `json:"State,omitempty" bson:"State,omitempty"`
-	Country       	string `json:"Country,omitempty" bson:"Country,omitempty"`
-	ZipCode       	string `json:"ZipCode,omitempty" bson:"ZipCode,omitempty"`	
-	CarrierCode     string `json:"CarrierCode,omitempty" bson:"CarrierCode,omitempty"`
-	ClassOfMail     string `json:"ClassOfMail,omitempty" bson:"ClassOfMail,omitempty"`
-	status     		string `json:"status,omitempty" bson:"status,omitempty"`
+	OrderID     string `json:"orderID,omitempty" bson:"orderID,omitempty"`
+	CompanyName string `json:"companyName,omitempty" bson:"companyName,omitempty"`
+	Address1    string `json:"address1,omitempty" bson:"address1,omitempty"`
+	Address2    string `json:"address2,omitempty" bson:"address2,omitempty"`
+	Address3    string `json:"address3,omitempty" bson:"address3,omitempty"`
+	City        string `json:"city,omitempty" bson:"city,omitempty"`
+	State       string `json:"state,omitempty" bson:"state,omitempty"`
+	Country     string `json:"country,omitempty" bson:"country,omitempty"`
+	ZipCode     string `json:"zipCode,omitempty" bson:"zipCode,omitempty"`
+	CarrierCode string `json:"carrierCode,omitempty" bson:"carrierCode,omitempty"`
+	ClassOfMail string `json:"classOfMail,omitempty" bson:"classOfMail,omitempty"`
+	Status      string `json:"status,omitempty" bson:"status,omitempty"`
 }
